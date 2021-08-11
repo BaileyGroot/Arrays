abfb1f766c302d195df9b88023d805a3dfcd3393
Grootrix <grootrix@gmail.com>
2021-08-11 10:23:04 +0200

    Add project files.

---
+++ Arrayss/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Arrayss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[9];
+
+            for (int i = 0; i < 9; i++)
+            {
+                numbers[i] = i+1;
+                if (i == 5)
+                {
+                    numbers[i] = numbers[i - 1] * 2;
+                }
+            }
+
+            foreach (int tal in numbers)
+            {
+                Console.WriteLine(tal);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
